b76f2d67f5f02f256284d349adc3d8c20cc86a44
Ashley Williams <ashley666ashley@gmail.com>
2023-01-05 07:13:17 -0600

    test: tweak load, add write for local assets

--- tests/write.rs
+++ tests/write.rs
@@ -1,15 +1,20 @@
 use std::collections::HashMap;
+use std::fs;
 use std::path::Path;
 
 use assert_fs::prelude::*;
+use image::ImageFormat;
 
 #[test]
-fn it_loads_local_assets() {
+fn it_writes_local_assets() {
     let origin = assert_fs::TempDir::new().unwrap();
+    let dest = assert_fs::TempDir::new().unwrap();
+    let dest_dir = Path::new(dest.to_str().unwrap());
 
     let mut files = HashMap::new();
     files.insert("README.md", "# axoasset");
     files.insert("styles.css", "@import");
+    files.insert("logo.png", "");
 
     for (file, contents) in files {
         let asset = origin.child(file);
@@ -20,9 +25,15 @@ fn it_loads_local_assets() {
         let loaded_asset = axoasset::load(origin_path).unwrap();
 
         if let axoasset::Asset::LocalAsset(asset) = loaded_asset {
-            assert!(std::str::from_utf8(&asset.contents)
-                .unwrap()
-                .contains(contents));
+            asset.write(dest.to_str().unwrap()).unwrap();
+            let written_file = dest_dir.join(file);
+            assert!(written_file.exists());
+            if asset.origin_path.contains("png") {
+                let format = ImageFormat::from_path(written_file).unwrap();
+                assert_eq!(format, ImageFormat::Png);
+            } else {
+                fs::read_to_string(written_file).unwrap().contains(contents);
+            }
         }
     }
 }
