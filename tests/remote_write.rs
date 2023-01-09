81fbd3070f70221785da7919e2a3d30b4a68b015
Ashley Williams <ashley666ashley@gmail.com>
2023-01-09 16:57:33 -0600

    test: remote write

--- tests/remote_write.rs
+++ tests/remote_write.rs
@@ -1,39 +1,48 @@
 use std::collections::HashMap;
 use std::fs;
-use std::path::Path;
 
-use assert_fs::prelude::*;
-use image::ImageFormat;
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, MockServer, ResponseTemplate};
 
 #[tokio::test]
 async fn it_writes_remote_assets() {
-    let origin = assert_fs::TempDir::new().unwrap();
+    let mock_server = MockServer::start().await;
+
     let dest = assert_fs::TempDir::new().unwrap();
-    let dest_dir = Path::new(dest.to_str().unwrap());
 
-    let mut files = HashMap::new();
-    files.insert("README.md", "# axoasset");
-    files.insert("styles.css", "@import");
-    files.insert("logo.png", "");
+    let mut assets = HashMap::new();
+    assets.insert("/README.md", "# axoasset");
+    assets.insert("/README", "# axoasset");
+    assets.insert("/styles.css", "@import");
+    assets.insert("/styles", "@import");
 
-    for (file, contents) in files {
-        let asset = origin.child(file);
-        let content = Path::new("./tests/assets").join(file);
-        asset.write_file(&content).unwrap();
+    for (route, contents) in assets {
+        let response = if route.contains("README") {
+            let readme_bytes = fs::read("./tests/assets/README.md").unwrap();
+            ResponseTemplate::new(200)
+                .set_body_bytes(readme_bytes)
+                .insert_header("Content-Type", "text/plain+md")
+        } else {
+            let styles_bytes = fs::read("./tests/assets/styles.css").unwrap();
+            ResponseTemplate::new(200)
+                .set_body_bytes(styles_bytes)
+                .insert_header("Content-Type", "text/css")
+        };
 
-        let origin_path = asset.to_str().unwrap();
-        let loaded_asset = axoasset::load(origin_path).await.unwrap();
+        Mock::given(method("GET"))
+            .and(path(route))
+            .respond_with(response)
+            .mount(&mock_server)
+            .await;
 
-        if let axoasset::Asset::LocalAsset(asset) = loaded_asset {
-            asset.write(dest.to_str().unwrap()).unwrap();
-            let written_file = dest_dir.join(file);
-            assert!(written_file.exists());
-            if asset.origin_path.contains("png") {
-                let format = ImageFormat::from_path(written_file).unwrap();
-                assert_eq!(format, ImageFormat::Png);
-            } else {
-                fs::read_to_string(written_file).unwrap().contains(contents);
-            }
+        let mut origin_path = format!("http://{}", mock_server.address());
+        origin_path.push_str(route);
+        let loaded_asset = axoasset::load(&origin_path).await.unwrap();
+
+        if let axoasset::Asset::RemoteAsset(asset) = loaded_asset {
+            let dest = asset.write(dest.to_str().unwrap()).await.unwrap();
+            assert!(dest.exists());
+            fs::read_to_string(dest).unwrap().contains(contents);
         }
     }
 }
