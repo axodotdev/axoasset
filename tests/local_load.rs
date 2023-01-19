69a5b3b319b972e07ad607708b77853d09d55323
Ashley Williams <ashley666ashley@gmail.com>
2023-01-19 15:59:25 -0600

    feat(load): add local as byte and as string tests

--- tests/local_load.rs
+++ tests/local_load.rs
@@ -26,3 +26,45 @@ async fn it_loads_local_assets() {
         }
     }
 }
+
+#[tokio::test]
+async fn it_loads_local_assets_as_bytes() {
+    let origin = assert_fs::TempDir::new().unwrap();
+
+    let mut files = HashMap::new();
+    files.insert("README.md", "# axoasset");
+    files.insert("styles.css", "@import");
+
+    for (file, contents) in files {
+        let asset = origin.child(file);
+        let content = Path::new("./tests/assets").join(file);
+        asset.write_file(&content).unwrap();
+
+        let origin_path = asset.to_str().unwrap();
+        let loaded_bytes = axoasset::load_bytes(origin_path).await.unwrap();
+
+        assert!(std::str::from_utf8(&loaded_bytes)
+            .unwrap()
+            .contains(contents));
+    }
+}
+
+#[tokio::test]
+async fn it_loads_local_assets_as_strings() {
+    let origin = assert_fs::TempDir::new().unwrap();
+
+    let mut files = HashMap::new();
+    files.insert("README.md", "# axoasset");
+    files.insert("styles.css", "@import");
+
+    for (file, contents) in files {
+        let asset = origin.child(file);
+        let content = Path::new("./tests/assets").join(file);
+        asset.write_file(&content).unwrap();
+
+        let origin_path = asset.to_str().unwrap();
+        let loaded_string = axoasset::load_string(origin_path).await.unwrap();
+
+        assert!(loaded_string.contains(contents))
+    }
+}
