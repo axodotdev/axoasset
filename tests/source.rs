2630efc982262f2845462a5a8dc19473a360790b
Aria Beingessner <a.beingessner@gmail.com>
2023-05-22 19:00:40 -0400

    add toml-edit integration

--- tests/source.rs
+++ tests/source.rs
@@ -29,3 +29,132 @@ fn substr_span_invalid() {
     let there_span = source.span_for_substr("there");
     assert_eq!(there_span, None);
 }
+
+#[cfg(feature = "json-serde")]
+#[test]
+fn json_valid() {
+    #[derive(serde::Deserialize, PartialEq, Eq, Debug)]
+    struct MyType {
+        hello: String,
+        goodbye: bool,
+    }
+
+    // Make the file
+    let contents = String::from(r##"{ "hello": "there", "goodbye": true }"##);
+    let source = axoasset::SourceFile::new("file.js", contents);
+
+    // Get the span for a non-substring (string literal isn't pointing into the String)
+    let val = source.deserialize_json::<MyType>().unwrap();
+    assert_eq!(
+        val,
+        MyType {
+            hello: "there".to_string(),
+            goodbye: true
+        }
+    );
+}
+
+#[cfg(feature = "json-serde")]
+#[test]
+fn json_invalid() {
+    #[derive(serde::Deserialize, PartialEq, Eq, Debug)]
+    struct MyType {
+        hello: String,
+        goodbye: bool,
+    }
+
+    // Make the file
+    let contents = String::from(r##"{ "hello": "there", "goodbye": true, }"##);
+    let source = axoasset::SourceFile::new("file.js", contents);
+
+    // Get the span for a non-substring (string literal isn't pointing into the String)
+    let res = source.deserialize_json::<MyType>();
+    assert!(res.is_err());
+}
+
+#[cfg(feature = "toml-serde")]
+#[test]
+fn toml_valid() {
+    #[derive(serde::Deserialize, PartialEq, Eq, Debug)]
+    struct MyType {
+        hello: String,
+        goodbye: bool,
+    }
+
+    // Make the file
+    let contents = String::from(
+        r##"
+hello = "there"
+goodbye = true
+"##,
+    );
+    let source = axoasset::SourceFile::new("file.toml", contents);
+
+    // Get the span for a non-substring (string literal isn't pointing into the String)
+    let val = source.deserialize_toml::<MyType>().unwrap();
+    assert_eq!(
+        val,
+        MyType {
+            hello: "there".to_string(),
+            goodbye: true
+        }
+    );
+}
+
+#[cfg(feature = "toml-serde")]
+#[test]
+fn toml_invalid() {
+    #[derive(serde::Deserialize, PartialEq, Eq, Debug)]
+    struct MyType {
+        hello: String,
+        goodbye: bool,
+    }
+
+    // Make the file
+    let contents = String::from(
+        r##"
+hello = "there"
+goodbye = 
+"##,
+    );
+    let source = axoasset::SourceFile::new("file.toml", contents);
+
+    // Get the span for a non-substring (string literal isn't pointing into the String)
+    let res = source.deserialize_toml::<MyType>();
+    assert!(res.is_err());
+}
+
+#[cfg(feature = "toml-edit")]
+#[test]
+fn toml_edit_valid() {
+    // Make the file
+    let contents = String::from(
+        r##"
+hello = "there"
+goodbye = true
+"##,
+    );
+    let source = axoasset::SourceFile::new("file.toml", contents);
+
+    // Get the span for a non-substring (string literal isn't pointing into the String)
+    let val = source.deserialize_toml_edit().unwrap();
+    assert_eq!(val["hello"].as_str().unwrap(), "there");
+    assert_eq!(val["goodbye"].as_bool().unwrap(), true);
+}
+
+#[cfg(feature = "toml-edit")]
+#[test]
+fn toml_edit_invalid() {
+    // Make the file
+    let contents = String::from(
+        r##"
+hello = "there"
+goodbye = 
+"##,
+    );
+    let source = axoasset::SourceFile::new("file.toml", contents);
+
+    // Get the span for a non-substring (string literal isn't pointing into the String)
+    let res = source.deserialize_toml_edit();
+    assert!(res.is_err());
+}
